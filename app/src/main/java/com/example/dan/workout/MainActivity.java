1f3fcc30788ba536f626843fac4e14cc7304ab78
Dan <github4dan@gmail.com>
2018-12-28 03:10:42 -0800

    Update sdk to 26

--- app/src/main/java/com/example/dan/workout/MainActivity.java
+++ app/src/main/java/com/example/dan/workout/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.dan.workout;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Vibrator;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
